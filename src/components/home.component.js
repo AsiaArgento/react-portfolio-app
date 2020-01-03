580c310b8c36c667db14daa27d90350dbdcc1c77
Matt M Moncada <misakamikotocsr@gmail.com>
2020-01-03 13:16:42 -0700

    added hover effects

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -10,23 +10,21 @@ export default class Home extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      isHover: false
-    };
+    this.state = {};
 
     this.onMouseEnterHandler = this.onMouseEnterHandler.bind(this);
     this.onMouseLeaveHandler = this.onMouseLeaveHandler.bind(this);
   }
 
-  onMouseEnterHandler() {
+  onMouseEnterHandler(key) {
     this.setState({
-      isHover: true
+      [key]: true
     });
   }
 
-  onMouseLeaveHandler() {
+  onMouseLeaveHandler(key) {
     this.setState({
-      isHover: false
+      [key]: false
     });
   }
 
@@ -37,16 +35,66 @@ export default class Home extends Component {
         <section className="info-section">
           <div className="logo">M</div>
           <div className="info-box-top">
-            <a onMouseEnter={this.onMouseEnterHandler} onMouseLeave={this.onMouseLeaveHandler} className="home active" href="/">
-              { this.state.isHover
+            <a
+              onMouseEnter={() => this.onMouseEnterHandler("home")}
+              onMouseLeave={() => this.onMouseLeaveHandler("home")}
+              className="home active"
+              id="one"
+              href="/"
+            >
+              { this.state.home
                   ? <span>Home</span>
                   : <FontAwesomeIcon icon={faHome} />
               }
             </a>
-            <a href="/about"><FontAwesomeIcon className="icon two" icon={faUser} /></a>
-            <a href="/skills"><FontAwesomeIcon className="icon three" icon={faCog} /></a>
-            <a href="/gallery"><FontAwesomeIcon className="icon four" icon={faEye} /></a>
-            <a href="/contact"><FontAwesomeIcon className="icon five" icon={faEnvelope} /></a>
+            <a
+              onMouseEnter={() => this.onMouseEnterHandler("about")}
+              onMouseLeave={() => this.onMouseLeaveHandler("about")}
+              className="about active"
+              id="two"
+              href="/about"
+            >
+              { this.state.about
+                  ? <span>About</span>
+                  : <FontAwesomeIcon icon={faUser} />
+              }
+            </a>
+            <a
+              onMouseEnter={() => this.onMouseEnterHandler("skills")}
+              onMouseLeave={() => this.onMouseLeaveHandler("skills")}
+              className="skills active"
+              id="three"
+              href="/skills"
+            >
+              { this.state.skills
+                  ? <span>Skills</span>
+                  : <FontAwesomeIcon icon={faCog} />
+              }
+            </a>
+            <a
+              onMouseEnter={() => this.onMouseEnterHandler("gallery")}
+              onMouseLeave={() => this.onMouseLeaveHandler("gallery")}
+              className="gallery active"
+              id="four"
+              href="/gallery"
+            >
+              { this.state.gallery
+                  ? <span>Gallery</span>
+                  : <FontAwesomeIcon icon={faEye} />
+              }
+            </a>
+            <a
+              onMouseEnter={() => this.onMouseEnterHandler("contact")}
+              onMouseLeave={() => this.onMouseLeaveHandler("contact")}
+              className="contact active"
+              id="five"
+              href="/contact"
+            >
+              { this.state.contact
+                  ? <span>Contact</span>
+                  : <FontAwesomeIcon icon={faEnvelope} />
+              }
+            </a>
           </div>
           <div className="info-box-bottom">
             <ul className="contact-links">
